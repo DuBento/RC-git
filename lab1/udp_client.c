c310701a678907db5e4f449f0feb33255fd97d5c
DuBento <duarte.bento2000@gmail.com>
2020-09-24 11:12:13 +0100

    udp

---
+++ lab1/udp_client.c
@@ -0,0 +1,51 @@
+// lixo
+
+#include <unistd.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <netdb.h>
+#include <string.h>
+#define PORT    "58001"
+#include <stdio.h>
+
+
+
+int main(){
+    int fd, errcode, len;
+    struct addrinfo hints, *res;
+    ssize_t n;
+    socklen_t addrlen;
+    char buffer[128];
+    char inbuff[128];
+    struct sockaddr_in addr;
+
+    fd = socket(AF_INET, SOCK_DGRAM, 0);
+    if(fd == -1) exit(1);
+
+    memset(&hints, 0, sizeof(hints));
+    memset(inbuff, 0, 128);
+    hints.ai_family=AF_INET;
+    hints.ai_socktype=SOCK_DGRAM;
+    
+    errcode = getaddrinfo("sigma03.tecnico.ulisboa.pt", PORT, &hints, &res);
+    if(errcode != 0) exit(1);
+
+
+    while((len = scanf("%s", inbuff))!=0){
+        n = sendto(fd, inbuff, 128, 0, res->ai_addr, res->ai_addrlen);
+        if (n == -1) exit(1);
+
+    }
+
+    addrlen = sizeof(addr);
+    n = recvfrom(fd, buffer, 128, 0, (struct sockaddr*) &addr, &addrlen);
+    if (n==-1) exit(1);
+
+    write(1, "echo: ", 6); write(1, buffer, n);
+
+    freeaddrinfo(res);
+    close(fd);
+}
+
---
+++ lab1/udp_server.c
@@ -0,0 +1,64 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+#include <sys/types.h>
+#include <sys/socket.h>
+
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <netdb.h>
+#include <string.h>
+
+#define PORT "58001"
+
+#define TRUE 1
+#define EXIT_FAILURE 1
+
+
+int fd, errcode;
+ssize_t n;
+socklen_t addrlen;
+struct addrinfo hints, *res;
+struct sockaddr_in addr;
+char buffer[128];
+
+int main() {
+    /*struct addrinfo hints;
+    int fd;*/
+
+    fd = socket(AF_INET, SOCK_DGRAM, 0);        //UDP SOCKET
+    if (fd == -1)
+            exit(EXIT_FAILURE);
+
+    memset(&hints, 0, sizeof(hints));
+    hints.ai_family = AF_INET;              // IPv4
+    hints.ai_family = SOCK_DGRAM;
+    hints.ai_flags = AI_PASSIVE;
+
+
+    errcode = getaddrinfo(NULL, PORT, &hints, &res);
+    if ((errcode) != 0)
+        exit(EXIT_FAILURE);
+
+    if (bind(fd, res->ai_addr, res->ai_addrlen) == -1) /*im in*/
+        exit(EXIT_FAILURE);
+
+    while (1) {
+        int n;
+        addrlen = sizeof(addr);
+        n = recvfrom(fd, buffer, 128, 0, (struct sockaddr*)&addr, &addrlen);
+        if (n == -1)
+            exit(EXIT_FAILURE);
+
+        write(1, "received: ", 10);
+        write(1, buffer, n);
+
+        n = sendto(fd, buffer, n, 0, (struct sockaddr*)&addr, addrlen);
+        if (n == -1)
+            exit(EXIT_FAILURE);
+    }
+
+    freeaddrinfo(res);
+    close(fd);
+}
